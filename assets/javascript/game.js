64a40a18b076df2331fb9b0f87cb2f023018ad07
Fernando Medina <fmedina771@gmail.com>
2017-05-07 08:10:27 -0400

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,29 +5,34 @@ var yourScore = 0;
 var crystalOne = 0;
 var crystalTwo = 0;
 var crystalThree = 0;
+var chrystalFour = 0;
 
 
 // Game Counters
 var wins = 0;
 var losses = 0;
 
-
-
-
 // FUNCTIONS
 //-------------------------------------------------------------
 function startGame() {
+    // Chooses random number between 19 and 120
     compScore = Math.floor(Math.random() * 120) + 19;
-    console.log(compScore);
+    console.log("compScore: " + compScore);
 
+    // Chooses random number for crystalOne between 1 and 12
     crystalOne = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalOne);
+    console.log("Crystal1: " + crystalOne);
 
+    // Chooses random number for crystalTwo between 1 and 12
     crystalTwo = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalTwo);
+    console.log("Crystal2: " + crystalTwo);
 
+    // Chooses random number for crystalThree between 1 and 12
     crystalThree = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalThree);
+    console.log("Crystal3: " + crystalThree);
+
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+    console.log("Crystal4: " + crystalFour);
 
     wins = 0;
     losses = 0;
@@ -36,7 +41,7 @@ function startGame() {
     function won() {
         alert("You Won!")
         wins++
-        document.querySelector("#wins").innerHTML = wins;
+        $('#wins').html(wins);
         reset();
     }
 
@@ -44,18 +49,17 @@ function startGame() {
     function lost() {
         alert("You Lost!")
         losses++
-        document.querySelector("#losses").innerHTML = losses;
+        $('#losses').html(losses);
         reset();
     }
 
-
-
-    // Crystal 1 function
+    // Crystal 1 click function
     $("#Crystal1").click(function() {
 
+        // Adds crystal scores together
         yourScore = yourScore + crystalOne;
-        console.log("Crystal1: " + yourScore);
-        // If yourScore is the same as compScore, run won function	
+        console.log(yourScore);
+        // If yourScore is the same as compScore, run won function  
         if (yourScore == compScore) {
             won();
         }
@@ -65,14 +69,17 @@ function startGame() {
 
         }
         // Selects Id yourScore from HTML and displays score selected from clicking first crystal
-        document.querySelector("#yourScore").innerHTML = yourScore;
+        $('#yourScore').html(yourScore);
+        // document.querySelector("#yourScore").innerHTML = yourScore;
 
     });
-    // Crystal 2 function
+    // Crystal 2 click function
     $("#Crystal2").click(function() {
+
+        // Adds crystal scores together
         yourScore = yourScore + crystalTwo;
-        console.log("Crystal2: " + yourScore);
-        // If yourScore is the same as compScore run won function	
+        console.log(yourScore);
+        // If yourScore is the same as compScore run won function   
         if (yourScore == compScore) {
             won();
         }
@@ -82,16 +89,17 @@ function startGame() {
 
         }
         // Selects Id yourScore from HTML and displays score selected from clicking second crystal
-
-        document.querySelector("#yourScore").innerHTML = yourScore;
+        $('#yourScore').html(yourScore);
+        // document.querySelector("#yourScore").innerHTML = yourScore;
 
     });
-    // Crystal 3 function
+    // Crystal 3 click function
     $("#Crystal3").click(function() {
 
+        // Adds crystal scores together
         yourScore = yourScore + crystalThree;
-        console.log("Crystal1: " + yourScore);
-        // If yourScore is the same as compScore run won function	
+        console.log(yourScore);
+        // If yourScore is the same as compScore run won function   
         if (yourScore == compScore) {
             won();
             // If your score is greater than compScore, run lost function
@@ -99,41 +107,70 @@ function startGame() {
             lost();
         }
         // Selects Id yourScore from HTML and displays score selected from clicking second crystal
-        document.querySelector("#yourScore").innerHTML = yourScore;
+        $('#yourScore').html(yourScore);
+        // document.querySelector("#yourScore").innerHTML = yourScore;
     });
-    document.querySelector("#yourScore").innerHTML = yourScore;
-    document.querySelector("#randomScore").innerHTML = compScore;
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#losses").innerHTML = losses;
+
+    // Crystal 4 click function
+    $("#Crystal4").click(function() {
+
+        // Adds crystal scores together
+        yourScore = yourScore + crystalFour;
+        console.log(yourScore);
+        // If yourScore is the same as compScore run won function   
+        if (yourScore == compScore) {
+            won();
+            // If your score is greater than compScore, run lost function
+        } else if (yourScore > compScore) {
+            lost();
+        }
+        // Selects Id yourScore from HTML and displays score selected from clicking second crystal
+        $('#yourScore').html(yourScore);
+        // document.querySelector("#yourScore").innerHTML = yourScore;
+    });
+
+
+    $('#yourScore').html(yourScore);
+    $('#randomScore').html(compScore);
+    $('#wins').html(wins);
+    $('#losses').html(losses);
+
+    // document.querySelector("#yourScore").innerHTML = yourScore;
+    // document.querySelector("#randomScore").innerHTML = compScore;
+    // document.querySelector("#wins").innerHTML = wins;
+    // document.querySelector("#losses").innerHTML = losses;
 }
 
+// Resets the game once you have either won or lost
 function reset() {
     yourScore = 0;
 
     compScore = Math.floor(Math.random() * 120) + 19;
-    console.log(compScore);
+    console.log("compScore: " + compScore);
 
     crystalOne = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalOne);
+    console.log("Crystal1: " + crystalOne);
 
     crystalTwo = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalTwo);
+    console.log("Crystal2: " + crystalTwo);
 
     crystalThree = Math.floor(Math.random() * 12) + 1;
-    console.log(crystalThree);
+    console.log("Crystal3: " + crystalThree);
 
-    document.querySelector("#yourScore").innerHTML = yourScore;
-    document.querySelector("#randomScore").innerHTML = compScore;
-    document.querySelector("#wins").innerHTML = wins;
-    document.querySelector("#losses").innerHTML = losses;
+    crystalFour = Math.floor(Math.random() * 12) + 1;
+    console.log("Crystal4: " + crystalFour);
+
+
+    $('#yourScore').html(yourScore);
+    $('#randomScore').html(compScore);
+    $('#wins').html(wins);
+    $('#losses').html(losses);
+
+    // document.querySelector("#yourScore").innerHTML = yourScore;
+    // document.querySelector("#randomScore").innerHTML = compScore;
+    // document.querySelector("#wins").innerHTML = wins;
+    // document.querySelector("#losses").innerHTML = losses;
 }
-
-
-
-
-
-
-
 
 // CALLING FUNCTIONS
 //-------------------------------------------------------------
